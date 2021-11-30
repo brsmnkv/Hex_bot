98abe0916b235c3d6ca1d03ebcd035a0d9c8fa53
d17037bm <boris.minkov@student.manchester.ac.uk>
2021-11-30 16:10:32 +0000

    Populate basic elements of mctsNode

--- agents/MCTSAgent/mctsNode.py
+++ agents/MCTSAgent/mctsNode.py
@@ -4,15 +4,29 @@ A MonteCarloTreeSearch Node class:
 - children are the existing / already created children nodes of that node
 - options are all the possible children nodes (MAYBE)
 - parent is the parent node - for backpropagation (none for root)
+- board - the state the node represents
 """
 
 class mctsNode:
 
-    def __init__(self, mctsNode _parent):
+    def __init__(self, _parent, _board):
         self.Q = 0
         self.N = 0
-        self.options = []
         self.children = []
         self.parent = _parent
 
+        self.options = []
         #generate options (MAYBE)
+
+        self.board = _board
+
+
+    def addChild(self, _child):
+        self.children.append(somechild)
+
+    def playout(self):
+        # playout from this node's board to end
+
+    def backpropagate(self):
+        # update this node's data and then backpropagate to its parent (unless root)    
+
