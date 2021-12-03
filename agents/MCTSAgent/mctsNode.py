41dcc095258f2273b183adaeaa39ba7fe41b69b9
d17037bm <boris.minkov@student.manchester.ac.uk>
2021-12-03 18:35:36 +0000

    basic mcts kind-of implemented. Things left to do: - Do something about branching factor (like big in the beginning and gradually decreasing or something smart like add a new branch at some random time stamps ) - Iterations are OK for testing, but need to be changed to time eventually. So that mcts iterates as much as it can but in a fixed time frame - See if the mcts even works? - Probably check any purely code places for improvement

--- agents/MCTSAgent/mctsNode.py
+++ agents/MCTSAgent/mctsNode.py
@@ -3,9 +3,11 @@ A MonteCarloTreeSearch Node class:
 - Q and N are total reward and visits to node, from basic algorithm
 - children are the existing / already created children nodes of that node
 - options are all the possible children nodes (MAYBE)
-- parent is the parent node - for backpropagation (none for root)
+- parent is the parent node - for backpropagation (None for root)
 - board - the state the node represents
 """
+
+from math import sqrt, log
 
 class mctsNode:
 
@@ -14,19 +16,41 @@ class mctsNode:
         self.N = 0
         self.children = []
         self.parent = _parent
+        self.leaf = True
+        self.size = len(_board)
 
         self.options = []
         #generate options (MAYBE)
 
+        # we want board to be a matrix of -1,0,1s
         self.board = _board
 
 
     def addChild(self, _child):
         self.children.append(somechild)
+        self.leaf = False
 
-    def playout(self):
-        # playout from this node's board to end
+    def getChildren(self):
+        return self.children
 
-    def backpropagate(self):
-        # update this node's data and then backpropagate to its parent (unless root)    
+    def isLeaf(self):
+        return self.leaf
 
+    def UCT(self, c):
+        return (self.Q + self.N) + c*sqrt(2*log(self.parent.N)/self.N)
+
+    def getBoard(self):
+        return self.board
+
+    def getOptions(self):
+        options = []
+        for i in range(self.size):
+            for j in range(self.size):
+                if(self.board[i][j] == 0):
+                    options.append((i,j))
+
+    # def playout(self):
+    #     # playout from this node's board to end
+
+    # def backpropagate(self):
+    #     # update this node's data and then backpropagate to its parent (unless root)    
---
+++ agents/MCTSAgent/mctsSearch.py
@@ -0,0 +1,143 @@
+from mctsNode import mctsNode
+import random
+
+c = 0.5
+
+fixedBranchFactor = 3
+
+
+def mctsSearch(root, iterations):
+    for i in range(iterations):
+        
+        # Selection
+        selectedNode = _treePolicy(root, c)
+
+        # Expansion
+
+        expanded = []
+
+        options = selectedNode.getOptions()
+
+        for i in range(fixedBranchFactor):
+            child, choice = _expandPolicy(selectedNode, options)
+            options.remove(choice)
+            selectedNode.addChild(child)
+
+            expanded.append(child)
+
+        # Playout & Backpropagate for each new child
+        
+        for child in expanded:
+            win = _playout(child)  
+            # win is 0 or 1
+
+            _backPropagate(child, win)
+
+
+
+def _treePolicy(root, c):
+    currentNode = root
+
+    while(not currentNode.isLeaf):
+        currentChildren = currentNode.getChildren()
+
+        # HERE WE DECIDE WHICH CHILD TO PICK ON EACH ITERATION
+        # IN THIS CASE WE DO THE GENERALLY AGREED UPON UCT
+        # AND THERE ISN'T MUCH TO WONDER ABOUT HERE
+        currentNode = max(currentChildren, key=lambda x: x.UCT(c))
+
+    return currentNode
+
+def _expandPolicy(selectedNode, options):
+
+    # HERE WE PICK THE NODE(S) TO EXPAND TO FROM THE SELECTED ONE
+    # FIRST WILL DO A RANDOMLY PICKED 3-5 RESULTING STATES
+
+    # IDEALLY HERE IS WHERE WE WANT A HEURISTIC FUNCTION / NN
+    # SO WE CAN BRANCH OUT TO THE IMPORTANT MOVES TO CONSIDER WITHOUT
+    # BRANCHING TO UNNECESSARY ONES
+
+    board = selectedNode.getBoard
+
+    choice = random.choice(options)
+
+    board[choice[0]][choice[1]] = 1
+    
+    nextNode = mctsNode(selectedNode, board)
+
+    return (nextNode,choice)
+
+def _playout(child):
+
+    board = child.getBoard()
+    options = child.getOptions()
+    colour = -1
+
+    # WE DO A RANDOM PLAYOUT POLICY
+    # THIS IS ANOTHER PLACE WHERE WE COULD IMPROVE STUFF
+    # BY DOING SOMETHING BETTER THAN RANDOM
+    # NOTE IM PLAYING OUT TILL FULL BOARD, BECAUSE IT MAY BE QUICKER TO DO SO,
+    # THAN CHECK FOR A WINNER AFTER EVERY MOVE (ROOM FOR ANALYSIS AND IMPROVEMENT)
+    while (options):
+        choice = random.choice(options)
+        options.remove(choice)
+        board[choice[0]][choice[1]] = colour
+        colour *= -1
+
+    return _winner(board)
+
+def _backPropagate(node, win):
+    node.N += 1
+    node.Q += win
+    if node.parent:
+        _backPropagate(node.parrent, win)
+
+
+
+def _winner(board):
+    # CHECK STARTING WITH THE TOP LINE IF THERE IS A CONNECTION TO BOTTOM
+    visited = {}
+    left = {}
+
+    for j in range(len(board)):
+        if(board[0][j] == 1):
+            left.add((0,j))
+
+    while left:
+        some = left.pop()
+        visited.add(some)
+
+        neighbours = _getNeighbours(board,some)
+            
+        for each in neighbours:
+            if(board[each[0]][each[1]] == 1):
+                if (each[0] == len(board)-1):
+                    return 1
+                if each not in visited:
+                    left.add(each)
+    return 0
+
+def _getNeighbours(board, ij):
+    x = ij[1]
+    y = ij[0]
+    #clockwise neighbours of a x,y
+    available = [True,True,True,True,True,True]
+    I_DISPLACEMENTS = [-1, -1, 0, 1,  1, 0]
+    J_DISPLACEMENTS = [0,  1,  1, 0, -1, -1]
+    if(y == 0): 
+        available[0] = False
+        available[1] = False
+    elif(y == len(board)-1):
+        available[3] = False
+        available[4] = False
+    if(x == 0):
+        available[4] = False
+        available[5] = False
+    elif(x == len(board)-1):
+        available[1] = False
+        available[2] = False
+    results = []
+    for i in range(6):
+        if available[i]:
+            result.append(y+I_DISPLACEMENTS[i], x+J_DISPLACEMENTS[i])
+    return results
